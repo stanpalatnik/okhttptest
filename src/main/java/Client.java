752624688b1a201ec6f0f07c50bfacadc3fc91b5
Stanislav Palatnik <stan@spalatnik.com>
2016-07-11 12:54:39 -0400

    Working download example

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,7 +3,10 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import javax.net.ssl.*;
 import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Client {
@@ -13,42 +16,37 @@ public class Client {
     private final String resource;
 
     public static void main(String args[]) throws IOException {
-        Client c = new Client("localhost", 7272, "26438F65-0827-4D7A-B113-C640199B52EC", "archive-staging-customer123");
-        c.getKey("12B07BCBX");
+        Client c = new Client("localhost", 7272, "08391BC4-8717-499A-8720-902EC828EAEF", "archive-staging-customer123");
+        c.getKey("key1");
     }
 
     public Client(String hostname, int port, String token, String resource) {
-        System.setProperty("https.protocols", "TLSv1");
         this.hostname = hostname;
         this.port = port;
         this.token = token;
         this.resource = resource;
     }
 
-    private final OkHttpClient client = new OkHttpClient();
+    private final OkHttpClient client = getUnsafeOkHttpClient();
     private final Gson gson = new Gson();
 
-    /* public void run() throws Exception {
-        Request request = new Request.Builder()
-                .url("https://api.github.com/gists/c2a7c39532239ff261be")
-                .build();
-        Response response = client.newCall(request).execute();
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-        Gist gist = gson.fromJson(response.body().charStream(), Gist.class);
-        for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue().content);
-        }
-    }          */
 
     public void getKey(String keyId) throws IOException {
         final String INIT_CALL = String.format("https://%s:%d/restapi/json/v1/resources/resourcename/%s/accounts/accountname/%s?AUTHTOKEN=%s",
                 hostname, port, resource, keyId, token);
         Response response = call(INIT_CALL);
-        System.out.println( response.body().string());
-        Wrapper gist = gson.fromJson(response.body().charStream(), Wrapper.class);
-        gist.toString();
+        InfoOperationWrapper infoResult = gson.fromJson(response.body().charStream(), InfoOperationWrapper.class);
+        Integer resourceId = infoResult.operation.Details.get("RESOURCEID");
+        Integer accountId = infoResult.operation.Details.get("ACCOUNTID");
+        downloadByAccountId(resourceId, accountId);
+    }
+    
+    private Key downloadByAccountId(Integer resourceId, Integer accountId) throws IOException {
+        final String DOWNLOAD_CALL = String.format("https://%s:%d/restapi/json/v1/resources/%d/accounts/%d/downloadfile?AUTHTOKEN=%s", 
+                hostname, port, resourceId, accountId, token);
+        Response response = call(DOWNLOAD_CALL);
+        Key key = gson.fromJson(response.body().charStream(), Key.class);
+        return key;
     }
 
     private Response call(String url) throws IOException {
@@ -59,14 +57,63 @@ public class Client {
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
         return response;
     }
+    
+    
 
-    static class Wrapper {
-        Operation op;
+    static class InfoOperationWrapper {
+        InfoOperation operation;
     }
 
-    static class Operation {
+    static class InfoOperation {
         String name;
         Map<String, String> result;
-        Map<String, Integer> details;
+        Map<String, Integer> Details;
+    }
+
+    static class Key {
+        String passphrase;
+        String key;
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains 
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager 
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager 
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+
+            OkHttpClient okHttpClient = builder.build();
+            return okHttpClient;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
