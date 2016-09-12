dc0cef5e85eb76acece0fe8c7f8b5c4d7e41b42f
Stanislav Palatnik <stan@spalatnik.com>
2016-09-12 16:40:09 -0400

    Add new API call

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,8 +6,7 @@ import okhttp3.Response;
 import javax.net.ssl.*;
 import java.io.IOException;
 import java.security.cert.CertificateException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Client {
     private final String hostname;
@@ -15,10 +14,13 @@ public class Client {
     private final String token;
     private final String resource;
 
-    public static void main(String args[]) throws IOException {
-        Client c = new Client("localhost", 7272, "08391BC4-8717-499A-8720-902EC828EAEF", "archive-staging-customer123");
-        c.getKey("key1");
-    }
+    private final OkHttpClient client = getUnsafeOkHttpClient();
+    private final Gson gson = new Gson();
+    private final Map<String, Key> keyMap = new HashMap<String, Key>();
+
+    private final String LIST_CALL_STRING = "https://%s:%d/restapi/json/v1/resources?AUTHTOKEN=%s";
+    private final String INFO_CALL_STRING = "https://%s:%d/restapi/json/v1/resources/resourcename/%s/accounts/accountname/%s?AUTHTOKEN=%s";
+    private final String DOWNLOAD_CALL_STRING = "https://%s:%d/restapi/json/v1/resources/%d/accounts/%d/downloadfile?AUTHTOKEN=%s";
 
     public Client(String hostname, int port, String token, String resource) {
         this.hostname = hostname;
@@ -27,26 +29,62 @@ public class Client {
         this.resource = resource;
     }
 
-    private final OkHttpClient client = getUnsafeOkHttpClient();
-    private final Gson gson = new Gson();
-
-
-    public void getKey(String keyId) throws IOException {
-        final String INIT_CALL = String.format("https://%s:%d/restapi/json/v1/resources/resourcename/%s/accounts/accountname/%s?AUTHTOKEN=%s",
-                hostname, port, resource, keyId, token);
-        Response response = call(INIT_CALL);
-        InfoOperationWrapper infoResult = gson.fromJson(response.body().charStream(), InfoOperationWrapper.class);
-        Integer resourceId = infoResult.operation.Details.get("RESOURCEID");
-        Integer accountId = infoResult.operation.Details.get("ACCOUNTID");
-        downloadByAccountId(resourceId, accountId);
+    public static void main(String args[]) throws IOException {
+        Client c = new Client(args[0], Integer.parseInt(args[1]), args[2], args[3]);
+        for(String i : c.listResources()) System.out.println(i);
     }
-    
+
+    /**
+     * Return a list of all resource names for this account. 
+     * We assume the resource name will be the key id 
+     * @return
+     * @throws IOException
+     */
+    public HashSet<String> listResources() throws IOException {
+        final String INFO_CALL = String.format(LIST_CALL_STRING,
+                hostname, port, token);
+        Response response = call(INFO_CALL);
+        ListOperationWrapper listResult = gson.fromJson(response.body().charStream(), ListOperationWrapper.class);
+        HashSet<String> keyResources = new HashSet<String>();
+
+        if (listResult!=null) {
+            for(Map<String, String> keyDetail : listResult.operation.Details) {
+                keyResources.add(keyDetail.get("RESOURCE NAME"));
+            }
+        }
+        else
+            throw new IOException("Response from PMP server listing all reources returned null");
+        return keyResources;
+    }
+
+    public Key getKey(String keyId) throws IOException {
+        Integer resourceId=null;
+        Integer accountId=null;
+        Key foundKey = keyMap.get(keyId);
+        if (foundKey == null) {
+            final String INFO_CALL = String.format(INFO_CALL_STRING,
+                    hostname, port, resource, keyId, token);
+            Response response = call(INFO_CALL);
+            InfoOperationWrapper infoResult = gson.fromJson(response.body().charStream(), InfoOperationWrapper.class);
+
+            if (infoResult!=null) {
+                resourceId = infoResult.operation.Details.get("RESOURCEID");
+                accountId = infoResult.operation.Details.get("ACCOUNTID");
+            }
+            else
+                throw new IOException("Response from PMP server for keyID: "+keyId+" returned null infoResult");
+
+            foundKey = downloadByAccountId(resourceId, accountId);
+            keyMap.put(keyId, foundKey);
+        }
+        return foundKey;
+    }
+
     private Key downloadByAccountId(Integer resourceId, Integer accountId) throws IOException {
-        final String DOWNLOAD_CALL = String.format("https://%s:%d/restapi/json/v1/resources/%d/accounts/%d/downloadfile?AUTHTOKEN=%s", 
+        final String DOWNLOAD_CALL = String.format(DOWNLOAD_CALL_STRING,
                 hostname, port, resourceId, accountId, token);
         Response response = call(DOWNLOAD_CALL);
-        Key key = gson.fromJson(response.body().charStream(), Key.class);
-        return key;
+        return gson.fromJson(response.body().charStream(), Key.class);
     }
 
     private Response call(String url) throws IOException {
@@ -57,28 +95,37 @@ public class Client {
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
         return response;
     }
-    
-    
 
-    static class InfoOperationWrapper {
+
+    private static class InfoOperationWrapper {
         InfoOperation operation;
     }
 
-    static class InfoOperation {
+    private static class InfoOperation {
         String name;
         Map<String, String> result;
         Map<String, Integer> Details;
     }
-
-    static class Key {
-        String passphrase;
-        String key;
+    
+    private static class ListOperationWrapper {
+        ListOperation operation;
+    }
+    
+    
+    private static class ListOperation {
+        String name;
+        Map<String, String> result;
+        Integer totalRows;
+        List<Map<String, String>> Details;    
     }
 
+    /* 
+        This is to deal with a bad SSL certificate during testing.
+     */
     private static OkHttpClient getUnsafeOkHttpClient() {
         try {
             // Create a trust manager that does not validate certificate chains 
-            final TrustManager[] trustAllCerts = new TrustManager[] {
+            final TrustManager[] trustAllCerts = new TrustManager[]{
                     new X509TrustManager() {
                         @Override
                         public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
@@ -115,5 +162,10 @@ public class Client {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public class Key {
+        String passphrase;
+        String key;
     }
 }
